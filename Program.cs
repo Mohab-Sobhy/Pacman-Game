ca1fe3760be008f177db76e7c1e2007c60eb0cbe
Mohab-Sobhy <132499733+Mohab-Sobhy@users.noreply.github.com>
2024-11-05 02:55:50 +0200

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,12 @@
+namespace Pacman_Game;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        GameInterface.WelcomePage();
+        Console.ReadKey(false);
+        GameEngine game = new GameEngine();
+        game.start();
+    }
+}
